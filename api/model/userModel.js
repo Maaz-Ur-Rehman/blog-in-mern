f71d61e1d38b9ff20c091e36aa100ad1a60b81a5
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-05-18 15:54:43 +0500

    create model

---
+++ api/model/userModel.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},{timestamps:true})
+
+const UserModel=mongoose.model('user',UserSchema)
+
+export default UserModel
