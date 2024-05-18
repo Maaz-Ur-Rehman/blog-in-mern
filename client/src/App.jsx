53517efc8677bfc5d51182253f076629ed462689
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-05-18 12:21:21 +0500

    add new change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SingUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Projects from './pages/Projects'
 const App = () => {
   return (
-    <h1 className='text-3xl col-span-1 text-cyan-100'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SingUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
