e12f374690cebc3bd767c2be96e042aa548eeaed
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-05-18 15:37:00 +0500

    db connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express=require('express')
 const app=express()
-
+const dotenv=require('dotenv')
+const mongoose=require('mongoose')
+dotenv.config()
+mongoose.connect( process.env.MONGO_URL)
+.then(()=>{
+    console.log('MongoDb is Connected')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const port=6000
 app.listen(port,()=>{
